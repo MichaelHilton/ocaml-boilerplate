42aacd7cae0c3b54deaa3d5de1c10957873c1466
yuanqing <hello@yuanqing.sg>
2015-04-03 21:02:08 +0800

    Use OUnit2

--- test.ml
+++ test.ml
@@ -1,9 +1,9 @@
 open Foo
-open OUnit
+open OUnit2
 
 let test_foo = ignore (run_test_tt_main ("foo" >::: [
 
-  "returns the specified argument" >:: (fun () ->
+  "returns the specified argument" >:: (fun _ ->
     assert_equal 2 (identity 2)
   )
 
